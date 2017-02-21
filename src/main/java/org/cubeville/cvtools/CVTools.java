14112fa8c4833846beca74bc5a2fe0a63cb90319
BuildTools <unconfigured@null.spigotmc.org>
2017-02-21 19:13:28 +0100

    Added rushed version of the no-growth function

--- src/main/java/org/cubeville/cvtools/CVTools.java
+++ src/main/java/org/cubeville/cvtools/CVTools.java
@@ -1,18 +1,28 @@
 package org.cubeville.cvtools;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.cubeville.commons.commands.CommandParser;
 
 import org.cubeville.cvtools.commands.*;
 
-public class CVTools extends JavaPlugin {
+public class CVTools extends JavaPlugin implements Listener {
 
     private CommandParser commandParser;
-    
+
+    Set<UUID> noGrowthWorlds;
+
     public void onEnable() {
         commandParser = new CommandParser();
         commandParser.addCommand(new ChatColor());
@@ -21,6 +31,12 @@ public class CVTools extends JavaPlugin {
         commandParser.addCommand(new CheckSign());
         commandParser.addCommand(new DelayedTask(this));
         commandParser.addCommand(new Info());
+
+        noGrowthWorlds = new HashSet<>();
+        noGrowthWorlds.add(UUID.fromString("f2d1566c-af98-4f1c-beb8-793a17deaf37"));
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -29,5 +45,12 @@ public class CVTools extends JavaPlugin {
         }
         return false;
     }
-        
+
+    @EventHandler
+    public void onBlockGrow(BlockGrowEvent event) {
+        if(noGrowthWorlds.contains(event.getBlock().getLocation().getWorld().getUID())) {
+            event.setCancelled(true);
+        }
+    }
+
 }

f6492f1320652ead1c75e81907bafebb36bb348b
BuildTools <frediw@users.noreply.github.com>
2018-10-15 15:53:27 +0200

    Double parameter list can have different delimiter now

--- src/main/java/org/cubeville/commons/commands/CommandParameterListDouble.java
+++ src/main/java/org/cubeville/commons/commands/CommandParameterListDouble.java
@@ -6,15 +6,26 @@ import java.util.List;
 
 public class CommandParameterListDouble implements CommandParameterType
 {
-    int length;
-
+    Integer length;
+    String delimiter;
+    
+    public CommandParameterListDouble(String delimiter) {
+        this.delimiter = delimiter;
+    }
+    
     public CommandParameterListDouble(int length) {
         this.length = length;
+        this.delimiter = ";";
+    }
+
+    public CommandParameterListDouble(int length, String delimiter) {
+        this.length = length;
+        this.delimiter = delimiter;
     }
     
     public boolean isValid(String value) {
-        String[] parts = value.split(";");
-        if(parts.length != length) return false;
+        String[] parts = value.split(delimiter);
+        if(length != null && parts.length != length) return false;
         try {
             for(int i = 0; i < parts.length; i++) {
                 Double.valueOf(parts[i]);
@@ -27,14 +38,14 @@ public class CommandParameterListDouble implements CommandParameterType
     }
 
     public String getInvalidMessage(String value) {
-        String[] parts = value.split(";");
-        if(parts.length != length) return value + " has not the correct length of " + length;
+        String[] parts = value.split(delimiter);
+        if(length != null && parts.length != length) return value + " has not the correct length of " + length;
         return "No valid number.";
     }
 
     public Object getValue(String value) {
         List<Double> ret = new ArrayList<>();
-        String[] parts = value.split(";");
+        String[] parts = value.split(delimiter);
         for(int i = 0; i < parts.length; i++) {
             ret.add(Double.valueOf(parts[i]));
         }

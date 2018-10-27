a8a9eb034c625a4a71bb59d21d6f9db4e2d8b60d
BuildTools <frediw@users.noreply.github.com>
2018-10-27 14:49:36 +0200

    - New "world" command parameter type - New killentities command - Commands can suppress non-error output on console now

---
+++ src/main/java/org/cubeville/commons/commands/CommandParameterWorld.java
@@ -0,0 +1,18 @@
+package org.cubeville.commons.commands;
+
+import org.bukkit.Bukkit;
+
+public class CommandParameterWorld implements CommandParameterType
+{
+    public boolean isValid(String value) {
+        return Bukkit.getWorld(value) != null;
+    }
+
+    public String getInvalidMessage(String value) {
+        return value + " is no existing world!";
+    }
+
+    public Object getValue(String value) {
+        return Bukkit.getWorld(value);
+    }
+}

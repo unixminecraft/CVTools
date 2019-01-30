dcfc6c45d92cc744c1932000fbbf4cc35c96bec3
unixminecraft <37056124+unixminecraft@users.noreply.github.com>
2019-01-29 21:25:19 -0600

    Re-update for CVChat.

--- src/main/java/org/cubeville/commons/utils/PlayerUtils.java
+++ src/main/java/org/cubeville/commons/utils/PlayerUtils.java
@@ -1,21 +1,7 @@
 package org.cubeville.commons.utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffectType;
-
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class PlayerUtils {
 
@@ -47,46 +33,5 @@ public class PlayerUtils {
 	    player.removePotionEffect(PotionEffectType.WATER_BREATHING);
 	    player.removePotionEffect(PotionEffectType.WEAKNESS);
 	    player.removePotionEffect(PotionEffectType.WITHER);
-	}
-	
-	public static Set<ProtectedRegion> getRegionsAtPlayerLocation(Player player) {
-	    Location location = player.getLocation();
-	    Vector vLocation = new Vector(location.getX(), location.getY(), location.getZ());
-	    WorldGuardPlugin worldGuard = (WorldGuardPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
-	    RegionManager regionManager = worldGuard.getRegionManager(location.getWorld());
-	    if(regionManager == null) { return null; }
-	    return regionManager.getApplicableRegions(vLocation).getRegions();
-	}
-	
-	@SuppressWarnings("unchecked")
-    public static List<Player> getPlayersOutsideRegion(ProtectedRegion region, World world) {
-	    Collection<Player> allPlayers = (Collection<Player>) Bukkit.getServer().getOnlinePlayers();
-	    List<Player> playersInsideRegion = getPlayersInsideRegion(region, world);
-	    List<Player> playersOutsideRegion = new ArrayList<Player>();
-	    for(Player player: allPlayers) {
-	        if(!playersInsideRegion.contains(player)) {
-	            playersOutsideRegion.add(player);
-	        }
-	    }
-	    return playersOutsideRegion;
-	}
-	
-	@SuppressWarnings("unchecked")
-    public static List<Player> getPlayersInsideRegion(ProtectedRegion region, World world) {
-	    Collection<Player> allPlayers = (Collection<Player>) Bukkit.getServer().getOnlinePlayers();
-	    List<Player> worldPlayers = new ArrayList<Player>();
-	    for(Player player: allPlayers) {
-	        if(player.getWorld().equals(world)) {
-	            worldPlayers.add(player);
-	        }
-	    }
-	    List<Block> regionBlocks = BlockUtils.getBlocksInWGRegion(world, region.getId(), Integer.MAX_VALUE);
-	    List<Player> regionPlayers = new ArrayList<Player>();
-	    for(Player player: worldPlayers) {
-	        if(regionBlocks.contains(player.getLocation().getBlock())) {
-	            regionPlayers.add(player);
-	        }
-	    }
-	    return regionPlayers;
 	}
 }
